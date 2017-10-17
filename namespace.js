c75bf54fefa0124199425bcd0f711bd800be667f
Admin <admin@Admins-MacBook-Pro.local>
2017-10-17 00:18:07 -0400

    final commit

--- namespace.js
+++ namespace.js
@@ -1,27 +1,22 @@
 var VIRA0016 = {
 
     init: function () {
-        var div = document.createElement('div'),
-        boxes = document.getElementById('boxes');
+        var div = document.createElement("div");
+        div.className = "box";
         div.textContent = "VIRA0016";
-        div.classList.add('div');
-        boxes.appendChild(div);
-        box.addEventListener('click', this.MClick);
-        box.addEventListener('mouseover', this.MOver);
-        box.addEventListener('mouseout', this.MOut);
+        document.getElementById("boxes").appendChild(div);
 
-        function MClick(ev) {
-            ev.currentTarget.style.borderColor = "#BBCA76";
-            ev.currentTarget.style.backgroundColor = "#11487E";
-            ev.currentTarget.style.fontSize=10px;
+        div.addEventListener("click", clickMe);
+        div.addEventListener("mouseover", hMouse);
+        div.addEventListener("mouseout", hMouse);
+
+        function clickMe(ev) {
+            this.style.backgroundColor = "yellow";
+            this.style.borderColor = "green";
         }
 
-        function MOver(ev) {
-            ev.currentTarget.classList.toggle('highlight');
-        }
-
-        function MOut(ev) {
-            ev.currentTarget.classList.toggle('highlight');
+        function hMouse(ev) {
+            ev.currentTarget.classList.toggle("highlight");
         }
     }
-};
+}
